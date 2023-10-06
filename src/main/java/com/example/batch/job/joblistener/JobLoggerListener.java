c551039b50ed8bcf72c470bea305f302383a8166
Anjaehyun <lvet0330@gmail.com>
2023-10-06 16:11:07 +0900

    feat : DB 데이터 이관하기
    orders에 있는 주문내역을 RepositoryItemReader를 통해 읽어온다
    정해놓은 chunk사이즈 만큼씩 데이터를 accounts 에 옮기는 작업 진행
     -> 전체 데이터가 8개여서 5개 옮기고 3개 추가로 옮김
    
    RepositoryItemWriter & ItemWrite 두 개 중 편한 것으로 사용하면 된다.

--- src/main/java/com/example/batch/job/joblistener/JobLoggerListener.java
+++ src/main/java/com/example/batch/job/joblistener/JobLoggerListener.java
@@ -1,27 +1,27 @@
-package com.example.batch.job.joblistener;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobExecutionListener;
-
-@Slf4j
-public class JobLoggerListener implements JobExecutionListener {
-    private static String BEFORE_MESSAGE = "{} job is Running";
-    private static String AFTER_MESSAGE = "{} job is Done. (Status: {})";
-
-    @Override
-    public void beforeJob(JobExecution jobExecution) {
-        log.info(BEFORE_MESSAGE, jobExecution.getJobInstance().getJobName());
-    }
-
-    @Override
-    public void afterJob(JobExecution jobExecution) {
-        log.info(AFTER_MESSAGE, jobExecution.getJobInstance().getJobName(), jobExecution.getStatus());
-
-        if(jobExecution.getStatus() == BatchStatus.FAILED){
-            //email 같은거 전송 가능
-            log.info("Job is Failed");
-        }
-    }
-}
+//package com.example.batch.job.joblistener;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.batch.core.BatchStatus;
+//import org.springframework.batch.core.JobExecution;
+//import org.springframework.batch.core.JobExecutionListener;
+//
+//@Slf4j
+//public class JobLoggerListener implements JobExecutionListener {
+//    private static String BEFORE_MESSAGE = "{} job is Running";
+//    private static String AFTER_MESSAGE = "{} job is Done. (Status: {})";
+//
+//    @Override
+//    public void beforeJob(JobExecution jobExecution) {
+//        log.info(BEFORE_MESSAGE, jobExecution.getJobInstance().getJobName());
+//    }
+//
+//    @Override
+//    public void afterJob(JobExecution jobExecution) {
+//        log.info(AFTER_MESSAGE, jobExecution.getJobInstance().getJobName(), jobExecution.getStatus());
+//
+//        if(jobExecution.getStatus() == BatchStatus.FAILED){
+//            //email 같은거 전송 가능
+//            log.info("Job is Failed");
+//        }
+//    }
+//}

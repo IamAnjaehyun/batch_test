16e0db026300ceb93ef1130af39e37caac857489
Anjaehyun <lvet0330@gmail.com>
2023-10-06 16:11:07 +0900

    Revert "faet : DB 데이터 이관하기"
    This reverts commit 5c97cf06245fc8548ce5b17524765b61f0bdce2d.

--- src/main/java/com/example/batch/core/domain/accounts/AccountsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.batch.core.domain.accounts;
-
-import com.example.batch.core.domain.orders.Orders;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountsRepository extends JpaRepository<Accounts, Integer> {
-}

15c198131bfe36728fe68168195ddd0d9dba63d8
Anjaehyun <lvet0330@gmail.com>
2023-10-06 15:15:36 +0900

    feat : 배치 전,후 로그 추가를 위한 리스너
    리스너 생성 후 오류 발생시 상태 및 메세지 정상 출력 확인

---
+++ src/main/java/com/example/batch/job/joblistener/JobListenerConfig.java
@@ -0,0 +1,47 @@
+package com.example.batch.job.joblistener;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+/**
+ * desc : Hello world 출력
+ * run: --spring.batch.job.name=JobListenerJob
+ */
+@Configuration
+@RequiredArgsConstructor
+public class JobListenerConfig {
+    @Bean
+    public Job JobListenerJob(JobRepository jobRepository, @Qualifier("JobListenerStep") Step step) {
+        return new JobBuilder("listenerJob", jobRepository)
+                .listener(new JobLoggerListener())
+                .start(step)
+                .build();
+    }
+
+    @Bean
+    public Step JobListenerStep(JobRepository jobRepository, @Qualifier("JobListenerTasklet") Tasklet tasklet, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("helloWorldStep", jobRepository)
+                .tasklet(tasklet, platformTransactionManager)
+                .allowStartIfComplete(true)
+                .build();
+    }
+
+    @Bean
+    public Tasklet JobListenerTasklet() {
+        return (((contribution, chunkContext) -> {
+//            System.out.println("=== JobListener is running ===");
+//            return RepeatStatus.FINISHED;
+            throw new Exception("Failed!");
+        }));
+    }
+}
--- src/main/java/com/example/batch/job/validatedparam/validator/FileParamValidator.java
+++ src/main/java/com/example/batch/job/validatedparam/validator/FileParamValidator.java
@@ -1,20 +1,20 @@
-package com.example.batch.job.validatedparam.validator;
-
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.JobParametersValidator;
-import org.springframework.util.StringUtils;
-
-import java.util.Objects;
-
-public class FileParamValidator implements JobParametersValidator {
-
-    @Override
-    public void validate(JobParameters parameters) throws JobParametersInvalidException {
-        String fileName = Objects.requireNonNull(parameters).getString("fileName");
-
-        if (!StringUtils.endsWithIgnoreCase(fileName, "csv")) {
-            throw new JobParametersInvalidException("this is not csv file");
-        }
-    }
-}
+//package com.example.batch.job.validatedparam.validator;
+//
+//import org.springframework.batch.core.JobParameters;
+//import org.springframework.batch.core.JobParametersInvalidException;
+//import org.springframework.batch.core.JobParametersValidator;
+//import org.springframework.util.StringUtils;
+//
+//import java.util.Objects;
+//
+//public class FileParamValidator implements JobParametersValidator {
+//
+//    @Override
+//    public void validate(JobParameters parameters) throws JobParametersInvalidException {
+//        String fileName = Objects.requireNonNull(parameters).getString("fileName");
+//
+//        if (!StringUtils.endsWithIgnoreCase(fileName, "csv")) {
+//            throw new JobParametersInvalidException("this is not csv file");
+//        }
+//    }
+//}

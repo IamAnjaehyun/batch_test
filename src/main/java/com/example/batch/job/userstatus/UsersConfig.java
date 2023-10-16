e402088ce868ffa6de35d5c6e86fcea5e14fdee0
IamAnJaeHyun <lvet0330@gmail.com>
2023-10-16 10:53:45 +0900

    feat : log 적용
    logback-spring.xml 적용하여 로그 적용
    
    log folder gitiginore에 추가
    
    log.info -> logger.info 변경

--- src/main/java/com/example/batch/job/userstatus/UsersConfig.java
+++ src/main/java/com/example/batch/job/userstatus/UsersConfig.java
@@ -1,15 +1,21 @@
 package com.example.batch.job.userstatus;
 
+import com.example.batch.BatchApplication;
 import com.example.batch.core.domain.users.Users;
 import com.example.batch.core.domain.users.UsersRepository;
 import com.example.batch.core.domain.users.type.Status;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
@@ -17,13 +23,16 @@ import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.data.RepositoryItemReader;
 import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Sort;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Collections;
 
@@ -35,25 +44,27 @@ import java.util.Collections;
 @Configuration
 @RequiredArgsConstructor
 public class UsersConfig {
-//    private static final int chunkSize = 5;
-//    private JobLauncher jobLauncher;
-//
-//
-//    @Autowired
-//    public UsersConfig(JobLauncher jobLauncher) {
-//        this.jobLauncher = jobLauncher;
-//    }
-//
-////    Job 실행 Scheduler
+    private static final int chunkSize = 5;
+    private JobLauncher jobLauncher;
+
+    private static final Logger logger= LoggerFactory.getLogger(BatchApplication.class);
+
+
+    @Autowired
+    public UsersConfig(JobLauncher jobLauncher) {
+        this.jobLauncher = jobLauncher;
+    }
+
+//    Job 실행 Scheduler
 //    @Scheduled(cron = "0 0 3 * * *") // 매일 오전 3시에 실행
-////    @Scheduled(cron = "1 * * * * *") // 매 분 1초마다 실행
-//    public void runUsersStepJob() throws Exception {
-//        JobParameters jobParameters = new JobParametersBuilder()
-//                .addString("Job 이름", "휴면유저 판별")
-//                .addString("작업 일시", String.valueOf(LocalDateTime.now())) // 현재 시간을 Parameter 로 추가
-//                .toJobParameters();
-//        jobLauncher.run(usersJob(null, null), jobParameters);
-//    }
+    @Scheduled(cron = "1 * * * * *") // 매 분 1초마다 실행
+    public void runUsersStepJob() throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("Job 이름", "휴면유저 판별")
+                .addString("작업 일시", String.valueOf(LocalDateTime.now())) // 현재 시간을 Parameter 로 추가
+                .toJobParameters();
+        jobLauncher.run(usersJob(null, null), jobParameters);
+    }
 
     //Step 을 실행 시키는 Job
     @Bean
@@ -102,7 +113,7 @@ public class UsersConfig {
 
                 if (daysBetween >= 7) { // 7일(1주) 이상
                     users.setStatus(Status.USER);
-                    log.info(users.getName() + " 님이 계정을 생성한 지 7 일이 경과 되어 " + Status.USER + "로 회원 상태를 변경하였습니다.");
+                    logger.info(users.getName() + " 님이 계정을 생성한 지 7 일이 경과 되어 " + Status.USER + "로 회원 상태를 변경하였습니다.");
                 }
             }
 
@@ -113,7 +124,7 @@ public class UsersConfig {
 
                 if (daysBetween >= 365) { // 1년(365일) 이상
                     users.setStatus(Status.OLD_USER);
-                    log.info(users.getName() + " 님의 마지막 접속 기록이 365 일 (이상) 경과 되어 " + Status.OLD_USER + "로 회원 상태를 변경하였습니다.");
+                    logger.info(users.getName() + " 님의 마지막 접속 기록이 365 일 (이상) 경과 되어 " + Status.OLD_USER + "로 회원 상태를 변경하였습니다.");
                 }
             }
             return users;
